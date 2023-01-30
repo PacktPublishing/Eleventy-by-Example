88cda1fa3acc407f9d16c9581889bff39f18c8cb
brob <bryanlrobinson@gmail.com>
2023-01-30 09:04:53 -0500

    Newest chapters

--- project-4/chapter-8/end/.eleventy.js
+++ project-4/chapter-8/end/.eleventy.js
@@ -1,6 +1,7 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const podcastTools = require('eleventy-plugin-podcast-tools');
 const algoliasearch = require('algoliasearch');
+const algoliasearchlite = require('algoliasearch/lite')
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");  
 
 require('dotenv').config();
@@ -13,20 +14,21 @@ module.exports = function(eleventyConfig) {
         return JSON.stringify(content);
     })
 
-    eleventyConfig.addFilter("algoliaPrep", (content) => {
+    eleventyConfig.addFilter("htmlStrip", (content) => {
         return content.replace(/(<([^>]+)>)/gi, "");
     })
 
-    eleventyConfig.addFilter("search", (query) => {
-        const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
-        const index = client.initIndex(process.env.INDEX_NAME);
-        return index.search(query, {
-            attributesToRetrieve: ["title", "url"],
-      
-          }).then(res => {
-            return res.hits;
-          })
-    })
+eleventyConfig.addFilter("search", (query) => {
+    const client = algoliasearchlite(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+    const index = client.initIndex(process.env.INDEX_NAME);
+    return index.search(query, {
+        attributesToRetrieve: ["title", "url"],
+    
+        }).then(res => {
+            console.log(JSON.stringify(res.hits, null, 2))
+        return res.hits;
+        })
+})
         
     eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {  
         name: "search", // The serverless function name for the permalink object 
@@ -34,18 +36,18 @@ module.exports = function(eleventyConfig) {
       });  
 
 
-    eleventyConfig.on('eleventy.after', async () => {
-        
-        // Short circuit if we're not in production according to Netlify
-        if (process.env.CONTEXT !== 'production') return
+eleventyConfig.on('eleventy.after', async () => {
+    
+    // Short circuit if we're not in production according to Netlify
+    if (process.env.CONTEXT !== 'production') return
 
-        const jsonContent = await require('./_site/algoliaIndex.json')
+    const jsonContent = await require('./_site/algoliaIndex.json')
 
-        const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
-        const index = client.initIndex(process.env.INDEX_NAME);
+    const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+    const index = client.initIndex(process.env.INDEX_NAME);
 
-        index.saveObjects(jsonContent)        
-    });
+    index.saveObjects(jsonContent)        
+});
     
 
 
---
+++ project-4/chapter-9/end/.eleventy.js
@@ -0,0 +1,106 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const podcastTools = require('eleventy-plugin-podcast-tools');
+const algoliasearch = require('algoliasearch');
+const algoliasearchlite = require('algoliasearch/lite')
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");  
+
+require('dotenv').config();
+module.exports = function(eleventyConfig) {
+
+
+    // Add data to the global data object
+    eleventyConfig.addGlobalData("hygraph", async function(){
+        const query = `query HygraphData {
+            episodes(orderBy: publishDate_DESC) {
+              audioFile {
+                id
+                url
+                size
+              }
+              publishDate
+              updatedAt
+              slug
+              title
+              body {
+                html
+                text
+              }
+            }
+          }`
+        const response = await fetch(process.env.HYGRAPH_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({query})
+            
+        })
+        const json = await response.json()
+        return json.data
+    })
+
+
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(podcastTools);
+
+
+    eleventyConfig.addFilter("jsDate", function(dateString) {
+        return new Date(dateString);
+    });
+
+    eleventyConfig.addFilter("jsonify", function(content) {
+        return JSON.stringify(content);
+    })
+
+    eleventyConfig.addFilter("htmlStrip", (content) => {
+        return content.replace(/(<([^>]+)>)/gi, "");
+    })
+
+eleventyConfig.addFilter("search", (query) => {
+    const client = algoliasearchlite(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+    const index = client.initIndex(process.env.INDEX_NAME);
+    return index.search(query, {
+        attributesToRetrieve: ["title", "url"],
+    
+        }).then(res => {
+            console.log(JSON.stringify(res.hits, null, 2))
+        return res.hits;
+        })
+})
+        
+    eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {  
+        name: "search", // The serverless function name for the permalink object 
+        functionsDir: "./netlify/functions/",  
+      });  
+
+
+eleventyConfig.on('eleventy.after', async () => {
+    
+    // Short circuit if we're not in production according to Netlify
+    if (process.env.CONTEXT !== 'production') return
+
+    const jsonContent = await require('./_site/algoliaIndex.json')
+
+    const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+    const index = client.initIndex(process.env.INDEX_NAME);
+
+    index.saveObjects(jsonContent)        
+});
+    
+
+
+
+    // Copy `assets/` to `_site/assets/`
+    eleventyConfig.addPassthroughCopy("assets");
+
+    // Set the source for 11ty to the /src directory
+    // Otherwise, this defaults to the project root
+    // This provides a cleaner project structure
+    return {
+        dir: {
+            input: "src",
+            output: "_site", // This is the default, but it's included here for clarity.
+            includes: "_templates"
+        }
+    }
+}
